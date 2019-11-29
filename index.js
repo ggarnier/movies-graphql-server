6b4cc37f37e24c8a9d580de54325d4d770f1b233
Guilherme Garnier <guilherme.garnier@gmail.com>
2019-11-29 11:35:07 -0300

    Convert to graphql types

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-var schema = buildSchema(`
-  type Query {
-    moviesByTitle(title: String): [Movie!]!
-    moviesByYear(year: Int): [Movie!]!
-  }
-  type Movie {
-    title: String!
-    year: Int!
-  }
-`);
+var graphql = require('graphql');
 
 var db = {
   movies: [
@@ -20,19 +9,43 @@ var db = {
   ],
 };
 
-var root = {
-  moviesByTitle: (args) => (
-    db.movies.filter(m => m.title.includes(args.title))
-  ),
-  moviesByYear: (args) => (
-    db.movies.filter(m => m.year === args.year)
-  ),
-};
+var movieType = new graphql.GraphQLObjectType({
+  name: 'Movie',
+  fields: {
+    title: { type: graphql.GraphQLString },
+    year: { type: graphql.GraphQLInt },
+  }
+});
+
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    moviesByTitle: {
+      type: new graphql.GraphQLList(movieType),
+      args: {
+        title: { type: graphql.GraphQLString }
+      },
+      resolve: (_, { title }) => (
+        db.movies.filter(m => m.title.includes(title))
+      ),
+    },
+    moviesByYear: {
+      type: new graphql.GraphQLList(movieType),
+      args: {
+        year: { type: graphql.GraphQLInt }
+      },
+      resolve: (_, { year }) => (
+        db.movies.filter(m => m.year === year)
+      ),
+    },
+  },
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
